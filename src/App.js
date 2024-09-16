19e92895655709ccb6949a649c4173f5c709212e
Imma~Nuel <Adesanyaoluwamuyiwa12@gmail.com>
2024-09-16 15:34:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import inspection from "./images/Inspection.png";
+
+function App() {
+  return (
+    <div>
+      <h1>Inspection Image:</h1>
+      <img src={inspection} alt="Inspection" />
+    </div>
+  );
+}
+
+export default App;
